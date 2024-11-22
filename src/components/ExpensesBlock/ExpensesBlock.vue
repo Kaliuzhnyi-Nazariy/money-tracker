4c6d258898c14f68d1133471bea8bead42309158
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-22 23:34:33 +0200

    start second block where will be shown all expenses

---
+++ src/components/ExpensesBlock/ExpensesBlock.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import StatItem from './StatItem.vue'
+
+const items = [
+  { title: 'new data', amount: 400, topic: 'food' },
+  { title: 'hata', amount: 150, topic: 'Health' },
+  { title: 'hata', amount: 220, topic: '' },
+  { amount: 12, topic: '' },
+]
+//I will add here all exoenses and then show how much user spend on each field
+</script>
+
+<template>
+  <div className="stats stats-vertical lg:stats-horizontal shadow">
+    <StatItem
+      v-for="item in items"
+      :key="item.title"
+      :title="item.title"
+      :amount="item.amount"
+      :topic="item.topic"
+    />
+  </div>
+</template>
---
+++ src/components/ExpensesBlock/StatItem.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const Props = defineProps({
+  title: { type: String, default: 'Others' },
+  amount: { type: Number },
+  topic: {
+    type: String,
+    default: 'others',
+  },
+})
+
+const compBackByTopic = computed(() => {
+  if (Props.topic === 'food') {
+    console.log('food')
+    return 'bg-green-400'
+  } else if (Props.topic === 'Health') {
+    return 'bg-orange-400'
+  } else if (Props.topic === '') {
+    return 'bg-gray-400'
+  } else {
+    return 'bg-gray-400'
+  }
+})
+</script>
+
+<template>
+  <div className="stat" :class="compBackByTopic">
+    <div className="stat-title text-white">{{ Props.title }}</div>
+    <div className="stat-value text-white">{{ Props.amount }}</div>
+    <div className="stat-desc text-white">{{ Props.topic }}</div>
+  </div>
+</template>
