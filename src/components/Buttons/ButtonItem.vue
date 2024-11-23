05ce3f3cd73151f5cd48a4ebbcb7b1934bd502b0
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-11-23 23:19:06 +0200

    buttons list

---
+++ src/components/Buttons/ButtonItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const Props = defineProps({
+  type: {
+    type: String,
+    default: 'button',
+  },
+  redirectTo: {
+    type: String,
+  },
+})
+
+const componentIs = computed(() => {
+  return Props.type === 'button' ? Props.type : 'a'
+})
+</script>
+
+<template>
+  <component
+    :is="componentIs"
+    :href="redirectTo"
+    class="flex justify-center w-full rounded-2xl border border-[#B4ABD0] bg-[#2C1B8B]"
+    ><slot>heeh</slot></component
+  >
+</template>
---
+++ src/components/Buttons/ButtonsList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ButtonItem from './ButtonItem.vue'
+</script>
+
+<template>
+  <ul class="flex flex-col gap-3 mt-3 w-[256px]">
+    <li>
+      <ButtonItem type="a" redirect-to="mailto:kaliuzhnyinazarii@gmail.com">Contact us</ButtonItem>
+    </li>
+    <li><ButtonItem @click="console.log('reset')">Reset info</ButtonItem></li>
+    <li><ButtonItem @click="console.log('Log out')">Log out</ButtonItem></li>
+  </ul>
+</template>
