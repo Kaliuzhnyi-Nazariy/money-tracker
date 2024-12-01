69fd11ca1228d0d8c02fe7dc5dffe7e1a2beddf4
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-01 10:51:26 +0200

    created input component'

---
+++ src/components/Input/InputComp.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: String,
+  label: String,
+  type: {
+    type: String,
+    default: 'text',
+  },
+})
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <label>
+    <span>{{ props.label }}</span>
+    <input
+      :type="props.type"
+      v-bind="$attrs"
+      :value="modelValue"
+      @input="emit('update:modelValue', ($event.target as HTMLInputElement)!.value)"
+      class="bg-white w-full px-2 py-1 text-black"
+    />
+  </label>
+</template>
