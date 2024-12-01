af3ee36f06719442fd62364cf0c09f2d55ed1a2d
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-01 10:52:45 +0200

    make dashboard

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import AllMoneyBlock from '@/components/AllMoneysBlock/AllMoneyBlock.vue'
+import ButtonsList from '@/components/Buttons/ButtonsList.vue'
+import ExpensesBlock from '@/components/ExpensesBlock/ExpensesBlock.vue'
+import ListMoney from '@/components/ListMoney.vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+</script>
+
+<template>
+  <button
+    @click="
+      () => {
+        userStore.register({
+          email: 'lxxx@mail.com',
+          password: 'AA12!@aa',
+          confirmPassword: 'AA12!@aa',
+        })
+      }
+    "
+  >
+    Click
+  </button>
+  <button
+    @click="
+      () => {
+        userStore.login({
+          enteredEmail: 'lxxx@mail.com',
+          password: 'AA12!@aa',
+        })
+      }
+    "
+  >
+    Click for login
+  </button>
+  <button
+    @click="
+      () => {
+        userStore.logout()
+      }
+    "
+  >
+    Click for logout
+  </button>
+
+  <AllMoneyBlock />
+  <ExpensesBlock />
+  <ListMoney type-of-list="expenses" />
+  <ListMoney type-of-list="earnings" />
+  <ButtonsList />
+</template>
