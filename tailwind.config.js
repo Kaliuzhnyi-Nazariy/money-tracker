edd32988687cd22c795d1a7c2b779e6b0753009c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-01 10:50:41 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#0C0E24',
+        second: '#2C1B8B',
+      },
+    },
   },
   plugins: [require('daisyui')],
 }
