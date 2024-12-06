ccfc0f5475e9f3bd55389e44166fac24da335490
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-06 10:45:29 +0200

    make universal Modal component

---
+++ src/components/IForm.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  screenSizeType: {
+    type: String,
+    required: true,
+    enum: ['mob', 'tablet', 'pc'],
+  },
+})
+
+const sizeStyle = computed(() => {
+  if (props.screenSizeType === 'mob') {
+    return 'lg:hidden w-[300px] min-h-[400px] '
+  } else {
+    return 'hidden lg:block w-[800px] h-[350px]'
+  }
+})
+</script>
+
+<template>
+  <div
+    class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white transition-all flex flex-col"
+    :class="sizeStyle"
+  >
+    <slot></slot>
+  </div>
+</template>
