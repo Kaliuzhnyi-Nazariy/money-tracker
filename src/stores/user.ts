c100dadfcc250edecc6b1e6474284f5820ce0e31
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-06 10:37:53 +0200

    persist user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,111 +1,117 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const base_API_URL = 'https://tracker-money-back.onrender.com/api/users'
 
-export const useUserStore = defineStore('user', () => {
-  const userEmail = ref('')
-  const userToken = ref('')
-  const userLoading = ref(false)
-  const userError = ref('')
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const userEmail = ref('')
+    const userToken = ref('')
+    const userLoading = ref(false)
+    const userError = ref('')
 
-  const turnToTrueUserLoading = () => {
-    userLoading.value = true
-  }
+    const refreshToken = computed(() => userToken.value)
 
-  const turnToFalseUserLoading = () => {
-    userLoading.value = false
-  }
-
-  const cleanError = () => {
-    userError.value = ''
-  }
-
-  async function register({
-    email,
-    password,
-    confirmPassword,
-  }: {
-    email: string
-    password: string
-    confirmPassword: string
-  }) {
-    // console.log(email, password, confirmPassword)
-    turnToTrueUserLoading()
-    cleanError()
-    if (password !== confirmPassword) {
-      turnToFalseUserLoading()
-      userError.value = 'Passwords do not match!'
-      return
+    const turnToTrueUserLoading = () => {
+      userLoading.value = true
     }
 
-    const res = await fetch(`${base_API_URL}/register`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json', // Specify the content type
-      },
-      body: JSON.stringify({
-        email,
-        password,
-        confirmPassword,
-      }),
-    })
-
-    if (!res.ok) {
-      const messageError = await res.json()
-      userError.value = messageError.message
-      turnToFalseUserLoading()
-      return
+    const turnToFalseUserLoading = () => {
+      userLoading.value = false
     }
 
-    console.log(await res.json())
-
-    turnToFalseUserLoading()
-  }
-
-  async function login({ enteredEmail, password }: { enteredEmail: string; password: string }) {
-    cleanError()
-    turnToTrueUserLoading()
-
-    const res = await fetch(`${base_API_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email: enteredEmail, password }),
-    })
-
-    if (!res.ok) {
-      const messageError = await res.json()
-      userError.value = messageError.message
-      turnToFalseUserLoading()
-
-      return
+    const cleanError = () => {
+      userError.value = ''
     }
 
-    const { email, token } = await res.json()
-    userToken.value = token
-    userEmail.value = email
-    turnToFalseUserLoading()
+    async function register({
+      email,
+      password,
+      confirmPassword,
+    }: {
+      email: string
+      password: string
+      confirmPassword: string
+    }) {
+      // console.log(email, password, confirmPassword)
+      turnToTrueUserLoading()
+      cleanError()
+      if (password !== confirmPassword) {
+        turnToFalseUserLoading()
+        userError.value = 'Passwords do not match!'
+        return
+      }
 
-    console.log({ email, token })
-    console.log({ userToken, userEmail })
-  }
+      const res = await fetch(`${base_API_URL}/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', // Specify the content type
+        },
+        body: JSON.stringify({
+          email,
+          password,
+          confirmPassword,
+        }),
+      })
 
-  async function logout() {
-    const res = await fetch(`${base_API_URL}/logout`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${userToken.value}` },
-    })
+      if (!res.ok) {
+        const messageError = await res.json()
+        userError.value = messageError.message
+        turnToFalseUserLoading()
+        return
+      }
 
-    if (!res.ok) {
+      console.log(await res.json())
+
+      turnToFalseUserLoading()
+    }
+
+    async function login({ enteredEmail, password }: { enteredEmail: string; password: string }) {
+      cleanError()
+      turnToTrueUserLoading()
+
+      const res = await fetch(`${base_API_URL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: enteredEmail, password }),
+      })
+
+      if (!res.ok) {
+        const messageError = await res.json()
+        userError.value = messageError.message
+        turnToFalseUserLoading()
+
+        return
+      }
+
+      const { email, token } = await res.json()
+      userToken.value = token
+      userEmail.value = email
+      turnToFalseUserLoading()
+
+      console.log({ email, token })
+      console.log({ userToken, userEmail })
+    }
+
+    async function logout() {
+      const res = await fetch(`${base_API_URL}/logout`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${userToken.value}` },
+      })
+
       console.log(res)
-      console.log('await res.json(): ', await res.json())
-      throw new Error('')
+      if (!res.ok) {
+        console.log('await res.json(): ', await res.json())
+        throw new Error('')
+      }
+
+      userToken.value = ''
     }
 
-    userToken.value = ''
-  }
-
-  return { userEmail, userToken, userLoading, userError, register, login, logout }
-})
+    return { userEmail, userToken, userLoading, userError, refreshToken, register, login, logout }
+  },
+  { persist: true },
+)
