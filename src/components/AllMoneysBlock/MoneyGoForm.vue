e9686fd2f6b633ebdcae423651a70f29bba1b432
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-06 10:47:54 +0200

    working on it

--- src/components/AllMoneysBlock/MoneyGoForm.vue
+++ src/components/AllMoneysBlock/MoneyGoForm.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { useCounterStore } from '@/stores/money'
+import { useCounterStore } from '@/stores/depositMoney'
 import { reactive } from 'vue'
+import InputComp from '../Input/InputComp.vue'
 
 const storeMoney = useCounterStore()
 
@@ -17,8 +18,16 @@ const handleSubmit = (e: Event) => {
 </script>
 
 <template>
-  <form @submit="handleSubmit">
-    <input type="text" v-model="withdrawData.amountToWithdraw" />
+  <form
+    @submit="handleSubmit"
+    class="absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%] bg-white p-6 text-black"
+  >
+    <h3 class="font-bold mb-2 text-center">What you buy?😏</h3>
+    <div class="flex flex-col gap-3">
+      <InputComp class="bg-black" placeholder="data" />
+      <input type="text" v-model="withdrawData.amountToWithdraw" />
+      <p>List of categories</p>
+    </div>
     <button type="submit">Withdraw</button>
   </form>
 </template>
