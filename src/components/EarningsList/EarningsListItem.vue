4e442a6a051c52b6e514a89499d191c3e36bdbd8
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-06 10:45:07 +0200

    upgrading component by adding modal and add necessary data

--- src/components/EarningsList/EarningsListItem.vue
+++ src/components/EarningsList/EarningsListItem.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useModal } from '../compossables/useModal'
+import IModal from '../IForm/IModal.vue'
+
 const Props = defineProps({
+  id: { type: String },
   name: {
     type: String,
     default: '',
@@ -17,17 +21,37 @@ const Props = defineProps({
     default: 'others',
   },
 })
+
+const { isModalOpen, openModal, closeModal } = useModal()
 </script>
 
 <template>
-  <li class="flex w-full justify-between py-1 px-2">
+  <li
+    class="flex w-full justify-between py-1 px-2"
+    @click="
+      () => {
+        openModal()
+        console.log(Props.id)
+      }
+    "
+  >
     <div class="flex flex-col">
       <h4 class="text-xl">{{ Props.name }}</h4>
-      <p class="opacity-50">{{ Props.category }}</p>
+      <!-- <p class="opacity-50">{{ Props.category }}</p> -->
     </div>
     <div class="flex flex-col">
       <p class="ml-auto">{{ Props.money }}$</p>
       <p class="opacity-50">{{ Props.date }}</p>
     </div>
   </li>
+  <IModal
+    :isOpen="isModalOpen"
+    @close="closeModal"
+    typeOfModal="depUpd"
+    :name
+    :money
+    :date
+    :category
+    :id
+  />
 </template>
