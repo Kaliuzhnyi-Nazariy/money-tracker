0d244a772f5b8ee43bff3b546bafc412439cc209
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-06 10:40:35 +0200

    make reg/log forms

---
+++ src/components/ListMoney/HeaderOfExpenssesList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useModal } from '../compossables/useModal'
+import IModal from '../IForm/IModal.vue'
+
+const { isModalOpen, openModal, closeModal } = useModal()
+</script>
+
+<template>
+  <div class="flex w-full justify-between px-2 pb-4">
+    <h3>Expenses list</h3>
+    <button class="mr-2" @click="openModal">+</button>
+  </div>
+  <IModal :isOpen="isModalOpen" @close="closeModal" typeOfModal="with"></IModal>
+</template>
--- src/components/LogForm/LogForm.vue
+++ src/components/LogForm/LogForm.vue
@@ -2,8 +2,10 @@
 import { reactive, ref, watch } from 'vue'
 import InputComp from '../Input/InputComp.vue'
 import { useUserStore } from '@/stores/user'
+import { useMutation } from '../compossables/useMutation'
+import router from '@/router'
 
-const regData = reactive({
+const logData = reactive({
   email: '',
   password: '',
 })
@@ -20,6 +22,16 @@ const useStore = useUserStore()
 
 const tempError = ref(false)
 
+const { mutation: logUser, isLoading: loggingUser } = useMutation({
+  mutationFn: (logData: { email: string; password: string; confirmPassword: string }) => {
+    return useStore.login({
+      enteredEmail: logData.email,
+      password: logData.password,
+    })
+  },
+  onSuccess: () => router.replace('/dashboard'),
+})
+
 watch(
   () => useStore.userError,
   () => {
@@ -35,21 +47,19 @@ watch(
 
 <template>
   <form
-    @submit.prevent="
-      useStore.login({
-        enteredEmail: regData.email,
-        password: regData.password,
-      })
-    "
+    @submit.prevent="logUser(logData)"
     :disabled="useStore.userLoading"
-    class="translate-x-[50%] flex flex-col gap-3 px-auto"
+    class="flex flex-col gap-3 px-auto"
     :class="useStore.userLoading ? 'bg-slate-800 p-5' : ''"
   >
-    <InputComp placeholder="Enter your email" v-model="regData.email" />
-    <InputComp placeholder="Enter your password" type="password" v-model="regData.password" />
+    <InputComp placeholder="Enter your email" v-model="logData.email" />
+    <InputComp placeholder="Enter your password" type="password" v-model="logData.password" />
     <button type="submit" :disabled="props.disabledButton" :is-loading="useStore.userLoading">
       Sign In
     </button>
     <div class="text-red-500" v-if="tempError">{{ useStore.userError }}</div>
   </form>
 </template>
+
+<!--     class="translate-x-[50%] flex flex-col gap-3 px-auto"
+ -->
