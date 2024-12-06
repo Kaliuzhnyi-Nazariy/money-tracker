763c620abf05c73151330e15c2c66cae1c8d3c6b
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-06 10:47:34 +0200

    added function of logging out

--- src/components/Buttons/ButtonsList.vue
+++ src/components/Buttons/ButtonsList.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { useUserStore } from '@/stores/user'
 import ButtonItem from './ButtonItem.vue'
+
+const userState = useUserStore()
 </script>
 
 <template>
@@ -8,6 +11,8 @@ import ButtonItem from './ButtonItem.vue'
       <ButtonItem type="a" redirect-to="mailto:kaliuzhnyinazarii@gmail.com">Contact us</ButtonItem>
     </li>
     <li><ButtonItem @click="console.log('reset')">Reset info</ButtonItem></li>
-    <li><ButtonItem @click="console.log('Log out')">Log out</ButtonItem></li>
+    <li>
+      <ButtonItem @click="userState.logout()">Log out</ButtonItem>
+    </li>
   </ul>
 </template>
