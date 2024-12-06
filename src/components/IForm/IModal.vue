951fc4f16f953dc0688fe9bf7001c70c80e1e315
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-06 10:41:27 +0200

    created IModal component

---
+++ src/components/IForm/IModal.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { computed, Teleport } from 'vue'
+import DepositMoneyFrom from '../AllMoneysBlock/DepositMoneyFrom.vue'
+
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+  },
+  typeOfModal: {
+    type: String,
+    default: 'dep',
+    enum: ['dep', 'upd', 'depUpd'],
+  },
+  id: {
+    type: String,
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+  money: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: String,
+    default: new Date().toLocaleDateString('fr-CA'),
+  },
+  category: {
+    type: String,
+    default: 'others',
+  },
+})
+
+const isModalOpen = computed(() => {
+  return props.isOpen ? 'fixed' : 'hidden'
+})
+
+const emit = defineEmits(['close'])
+</script>
+
+<template>
+  <component :is="Teleport" to="body">
+    <div
+      :class="isModalOpen"
+      class="w-full h-full top-0 left-0 -translate-y-50% bg-black bg-opacity-50"
+      @click.self="emit('close')"
+    >
+      <DepositMoneyFrom v-if="props.typeOfModal === 'dep'" @closeAfterSubmit="emit('close')" />
+      <!-- <MoneyGoForm v-else-if="props.typeOfModal === 'with'" /> -->
+      <DepositMoneyFrom
+        v-if="props.typeOfModal === 'depUpd'"
+        @closeAfterSubmit="emit('close')"
+        :id
+        :name
+        :money
+        :date
+        :category
+        type="upd"
+      />
+    </div>
+  </component>
+</template>
