c7987252d3d2a3eaa32d389c04ca933907265c6b
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-12-08 23:16:58 +0200

    made a form for withdraw operation

--- src/components/AllMoneysBlock/MoneyGoForm.vue
+++ src/components/AllMoneysBlock/MoneyGoForm.vue
@@ -1,33 +1,134 @@
 <script setup lang="ts">
-import { useCounterStore } from '@/stores/depositMoney'
-import { reactive } from 'vue'
+import { useExpensesMoney } from '@/stores/expensesMoney'
+import { computed, reactive } from 'vue'
 import InputComp from '../Input/InputComp.vue'
+import DatePicker from 'primevue/datepicker'
+import { dateFormatted } from '../compossables/dateFormatter'
 
-const storeMoney = useCounterStore()
+const storeMoney = useExpensesMoney()
 
-const withdrawData = reactive({
-  amountToWithdraw: '',
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'with',
+    enum: ['with', 'upd'],
+  },
+  id: {
+    type: String,
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+  money: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: String,
+    default: new Date().toLocaleDateString('fr-CA'),
+  },
+  category: {
+    type: String,
+    default: 'others',
+  },
 })
 
-const handleSubmit = (e: Event) => {
-  e.preventDefault()
+const emit = defineEmits(['close'])
 
-  storeMoney.withdrawMoney(Number(withdrawData.amountToWithdraw))
-  withdrawData.amountToWithdraw = ''
+export interface IWithdrawData {
+  date: Date | Date[] | (Date | null)[] | null | undefined | string
+  amountToWithdraw: string | number | undefined
+  category: string
+  title: string
 }
+
+const withdrawData = reactive({
+  date: null,
+  amountToWithdraw: '',
+  category: 'others',
+  title: '',
+}) as IWithdrawData
+
+const cleanDataValues = () => {
+  withdrawData.date = null
+  withdrawData.amountToWithdraw = ''
+  withdrawData.category = 'others'
+  withdrawData.title = ''
+}
+
+if (props.name) {
+  withdrawData.title = props.name
+  withdrawData.amountToWithdraw = props.money
+  withdrawData.category = props.category
+  withdrawData.date = props.date
+}
+
+const operation = computed(() => {
+  return props.type === 'with'
+    ? storeMoney.addExpenses({
+        title: withdrawData.title,
+        date: withdrawData.date,
+        price: withdrawData.amountToWithdraw,
+        type: 'expenses',
+        category: withdrawData.category,
+      })
+    : storeMoney.updateExpenses({
+        title: withdrawData.title,
+        date: withdrawData.date,
+        price: withdrawData.amountToWithdraw,
+        type: 'expenses',
+        category: withdrawData.category,
+      })
+})
 </script>
 
 <template>
   <form
-    @submit="handleSubmit"
+    @submit.prevent="
+      () => {
+        withdrawData.date = dateFormatted({ dateBefore: withdrawData.date })
+        operation
+        emit('close')
+        cleanDataValues()
+      }
+    "
     class="absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%] bg-white p-6 text-black"
   >
-    <h3 class="font-bold mb-2 text-center">What you buy?😏</h3>
+    <h3 class="font-bold mb-2 text-center">What you bought?😏</h3>
     <div class="flex flex-col gap-3">
-      <InputComp class="bg-black" placeholder="data" />
-      <input type="text" v-model="withdrawData.amountToWithdraw" />
-      <p>List of categories</p>
+      <small class="text-sm">What your purchase is?</small>
+      <InputComp type="text" v-model="withdrawData.title" placeholder="How much you spent? 😏" />
+      <small class="text-sm">How much you spent? 😏</small>
+      <InputComp
+        type="text"
+        v-model="withdrawData.amountToWithdraw"
+        placeholder="How much you spent? 😏"
+      />
+      <select class="bg-white w-full" v-model="withdrawData.category">
+        <option value="food">food</option>
+        <option value="clothes">clothes</option>
+        <option value="health">health</option>
+        <option value="others" default>others</option>
+      </select>
+      <DatePicker name="date" fluid v-model="withdrawData.date" class="mb-4" />
     </div>
-    <button type="submit">Withdraw</button>
+    <div class="flex justify-center" v-if="props.type === 'with'">
+      <button type="submit">Withdraw</button>
+    </div>
+    <div class="mx-auto flex flex-col" v-else>
+      <button type="submit">Update</button>
+      <button
+        type="button"
+        @click="
+          () => {
+            console.log(props.id)
+            storeMoney.deleteExpenses({ expensesId: props.id })
+          }
+        "
+      >
+        Delete
+      </button>
+    </div>
   </form>
 </template>
