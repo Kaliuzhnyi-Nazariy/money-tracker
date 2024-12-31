1d42004762f8bc127c4584720a207826314a67b0
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-01-01 00:29:10 +0200

    add button for reg and log comp

---
+++ src/components/ButtonLogReg/ButtonLogReg.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  logOrReg: {
+    type: String,
+    default: 'log',
+  },
+  isLoading: Boolean,
+})
+
+const textInButton = computed(() => {
+  return props.logOrReg === 'log' ? 'Sign in' : 'Sign up'
+})
+</script>
+
+<template>
+  <button
+    class="border border-white py-0.5 hover:border-transparent hover:bg-white hover:text-main transition-all"
+    :class="[props.isLoading ? 'cursor-wait bg-white bg-opacity-50' : '']"
+    :disabled="props.isLoading"
+  >
+    {{ textInButton }}
+  </button>
+</template>
