8660a011b140aae012040cc9ba1f4b2850b5be95
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-01-01 00:35:13 +0200

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,57 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import DashboardView from '@/views/DashboardView.vue'
 import GreetingView from '@/views/GreetingView.vue'
+import { useUserStore } from '@/stores/user'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: '/', name: 'greeting', component: GreetingView },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('@/views/DashboardView.vue'),
-    },
-    {
-      path: '/authorization',
-      name: 'authorization',
-      component: () => import('../views/AuthView.vue'),
-    },
-    {
-      path: '/forgot',
-      name: 'forgotPassword',
-      component: () => import('../views/ForgotPassword.vue'),
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
-  ],
+const routes = [
+  { path: '/', name: 'greeting', component: GreetingView },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('@/views/DashboardView.vue'),
+  },
+  {
+    path: '/authorization',
+    name: 'authorization',
+    component: () => import('../views/AuthView.vue'),
+  },
+  {
+    path: '/forgot',
+    name: 'forgotPassword',
+    component: () => import('../views/ForgotPassword.vue'),
+  },
+  {
+    path: '/renew',
+    name: 'renewPassword',
+    component: () => import('../views/ForgotPasswordForm.vue'),
+  },
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
 })
 
-export default router
+router.beforeEach((to, from, next) => {
+  // Access the user store
+  const userStore = useUserStore()
+  const { userToken } = userStore // Retrieve token state
+  const { name } = to // Target route name
+
+  // Ensure authentication flow is logical and avoids conflicting redirects
+  if (
+    !userToken &&
+    name !== 'authorization' &&
+    name !== 'forgotPassword' &&
+    name !== 'greeting' &&
+    name !== 'renewPassword'
+  ) {
+    // If not authenticated, redirect to the login page
+    next({ name: 'authorization' })
+  } else if (userToken && name === 'authorization') {
+    // If authenticated, prevent access to the login page
+    next({ name: 'dashboard' })
+  } else {
+    // Proceed to the intended route
+    next()
+  }
+})
