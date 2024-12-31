c11a21d8db72131995c6a22f2f8afc392eb0d668
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-01-01 00:58:38 +0200

    solve error with slot

--- src/components/ui/calendar/CalendarHeading.vue
+++ src/components/ui/calendar/CalendarHeading.vue
@@ -6,22 +6,28 @@ import { computed, type HTMLAttributes } from 'vue'
 const props = defineProps<CalendarHeadingProps & { class?: HTMLAttributes['class'] }>()
 
 const delegatedProps = computed(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { class: _, ...delegated } = props
-
   return delegated
 })
 
 const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
-<template>
+<!-- <template>
   <CalendarHeading
-    v-slot="{ headingValue }"
+    v-slot="{ headingValue }: { headingValue: unknown }"
     :class="cn('text-sm font-medium', props.class)"
     v-bind="forwardedProps"
   >
     <slot :heading-value>
       {{ headingValue }}
     </slot>
+  </CalendarHeading>
+</template> -->
+
+<template>
+  <CalendarHeading :class="cn('text-sm font-medium', props.class)" v-bind="forwardedProps">
+    <slot> </slot>
   </CalendarHeading>
 </template>
